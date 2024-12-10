0598db3c836de85fb50f2490fdd92d165daa98c2
Morri <acalberto@iesrafaelalberti.es>
2024-12-10 21:28:57 +0100

    Probando hilos

--- ThreadsRockPapersScissors/Program.cs
+++ ThreadsRockPapersScissors/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+static string PlayRPS(){
+List<string> lanzar = new List<string>{"Rock", "Paper", "Scissors"};
+
+Random rnd = new Random();
+
+int index = rnd.Next(0, lanzar.Count);
+
+return "Challenger played: " + lanzar[index]; ;
+};
+
+
+ThreadStart newThread = new ThreadStart();
